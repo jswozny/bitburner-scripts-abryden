8f2993cfa426b162babe92f568e98842bbbb5086
Jeremy Wozny <61649767+jswozny@users.noreply.github.com>
2026-03-11 10:00:15 -0400

    Update GitHub user and repository details in options

--- git-pull.js
+++ git-pull.js
@@ -1,12 +1,12 @@
 let options;
 const argsSchema = [
-    ['github', 'alainbryden'],
-    ['repository', 'bitburner-scripts'],
+    ['github', 'jswozny'],
+    ['repository', 'bitburner-scripts-alainbryde'],
     ['branch', 'main'],
     ['download', []], // By default, all supported files in the repository will be downloaded. Override with just a subset of files here
     ['new-file', []], // If a repository listing fails, only files returned by ns.ls() will be downloaded. You can add additional files to seek out here.
     ['subfolder', ''], // Can be set to download to a sub-folder that is not part of the remote repository structure
-    ['extension', ['.js', '.ns', '.txt', '.script']], // Files to download by extension
+    ['extension', ['.js', '.ts', '.txt', '.tsx','.json']], // Files to download by extension
     ['omit-folder', ['Temp/']], // Folders to omit when getting a list of files to update (TODO: This may be obsolete now that we get a list of files from github itself.)
 ];
 
